e03cbefcd6edebef5d1407476720b762d107bf2f
Andrew Davis <AndrewRDavis@users.noreply.github.com>
2016-01-22 14:45:16 -0500

    Final temperature calculation program from either F to C or vice versa.

--- temperature.rb
+++ temperature.rb
@@ -1,19 +1,25 @@
-def temperature()
-	
-puts "What temperature do you want to convert to (C or F)?"
-user_metric = gets.chomp
 puts "What is the current temperature?"
-user_temperature = gets.chomp
-
-if user_metric = C
-	then user_metric_reversed = "Fahrenheit"
-else
+user_input = gets.chomp
+user_input.downcase!
+degrees = user_input.gsub(/[^0-9]/, '').to_f
+if user_input.include? "c"
+	user_metric_reversed = "Fahrenheit"
+elsif user_input.include? "f"
 	user_metric_reversed = "Celsius"
-
-if user_metric = C
-	then unit_conversion = user_temperature * 1.8 + 32
 else
-	unit_conversion = (user_temperature - 32) / 1.8
-
-print "The temperature is #{unit_conversion} degrees in #{user_metric_reversed}"
+	puts "What unit is the current degrees your provided?"
+	user_metric = gets.chomp
+	user_metric.downcase!
+	if user_input.include? "c"
+		user_metric_reversed = "Fahrenheit"
+	else user_input.include? "f"
+		user_metric_reversed = "Celsius"
+	end
 end
+if user_input.include? "c" or user_metric.include? "c"
+	degrees_output = ((degrees * 1.8) + 32)
+else
+	degrees_output = ((degrees - 32) / 1.8)
+end
+
+print "The temperature is #{degrees_output} degrees in #{user_metric_reversed}"
